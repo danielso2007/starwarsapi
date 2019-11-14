8ed098879bf663a5f47180e7e73d32fc1df5dc59
Daniel Oliveira <danielso2007@gmail.com>
2019-11-14 03:48:52 -0300

    chore: Atualização de versão.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Iniciando API...
 cd target
-nohup java -jar starwarsapi-1.2.0-SNAPSHOT.jar & tail -f nohup.out
+nohup java -jar starwarsapi-1.4.0-SNAPSHOT.jar & tail -f nohup.out
